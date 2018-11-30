a6c1fcd32bacbf10dd742c2ddf7f0cbffd233ba2
ICEY <crazycz.cc@ckx-ily.cn>
2018-11-30 17:04:27 +0800

    Add one more testcase

--- test/TestDependencyResolver.cc
+++ test/TestDependencyResolver.cc
@@ -6,7 +6,9 @@ using namespace std;
 using namespace sona;
 using namespace ckx::Meta;
 
-int main() {
+void test0() {
+  cout << "-- running test0" << endl;
+
   string mod0 = "support";
   string mod1 = "chrono";
   string mod2 = "container";
@@ -26,7 +28,7 @@ int main() {
   DependInfo mod4depinfo(mod4, std::move(mod4req));
 
   vector<ref_ptr<DependInfo>> deplist {
-    &mod4depinfo, &mod1depinfo, &mod3depinfo, &mod2depinfo, &mod0depinfo,
+    &mod4depinfo, &mod1depinfo, &mod3depinfo, &mod2depinfo, &mod0depinfo
   };
 
   optional<vector<ref_ptr<DependInfo>>>
@@ -37,6 +39,37 @@ int main() {
       cout << dpinfo.get().GetExportedName() << endl;
     }
   }
+}
 
+void test1() {
+  cout << "-- running test1" << endl;
+
+  string mod0 = "fork";
+  string mod1 = "knife";
+
+  vector<string> mod0req { mod1 };
+  vector<string> mod1req { mod0 };
+
+  DependInfo mod0depinfo(mod0, std::move(mod0req));
+  DependInfo mod1depinfo(mod1, std::move(mod1req));
+
+  vector<ref_ptr<DependInfo>> deplist { &mod1depinfo, &mod0depinfo };
+
+  optional<vector<ref_ptr<DependInfo>>>
+    resolvedDeplist = ResolveDependency(deplist);
+
+  if (resolvedDeplist.has_value()) {
+    for (ref_ptr<DependInfo> dpinfo : resolvedDeplist.value()) {
+      cout << dpinfo.get().GetExportedName() << endl;
+    }
+  }
+  else {
+    cout << "no solution!" << endl;
+  }
+}
+
+int main() {
+  test0();
+  test1();
   return 0;
 }
