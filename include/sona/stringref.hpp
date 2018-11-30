579c69e2c52d8069b63e1b017ee50fc114829717
ICEY <crazycz.cc@ckx-ily.cn>
2018-11-30 21:59:35 +0800

    Add string ref

---
+++ include/sona/stringref.hpp
@@ -0,0 +1,61 @@
+#ifndef STRINGREF_HPP
+#define STRINGREF_HPP
+
+#include <string>
+#include <cstddef>
+#include <unordered_map>
+
+namespace sona {
+
+namespace impl_stc89c52 {
+extern thread_local std::unordered_map<std::string, int> glob_container;
+}
+
+class string_ref {
+public:
+  using string_set = std::unordered_map<std::string, int>;
+
+  string_ref(std::string &&str) noexcept {
+    pv = &(*(impl_stc89c52::glob_container.emplace(std::move(str), 1).first));
+  }
+
+  string_ref(std::string const& str) {
+    pv = &(*(impl_stc89c52::glob_container.emplace(str, 1).first));
+  }
+
+  string_ref(string_ref const& that) : pv(that.pv) {
+    that.pv->second++;
+  }
+
+  string_ref& operator= (string_ref const& that) noexcept {
+    pv = that.pv;
+    that.pv->second++;
+    return *this;
+  }
+
+  ~string_ref() {
+    pv->second--;
+    if (pv->second == 0) {
+      impl_stc89c52::glob_container.erase(pv->first);
+    }
+  }
+
+  std::string const& get() const noexcept {
+    return pv->first;
+  }
+
+  friend bool operator== (string_ref const& r1, string_ref const& r2) {
+    return r1.pv == r2.pv;
+  }
+
+  friend bool operator!= (string_ref const& r1, string_ref const& r2) {
+    return r1.pv == r2.pv;
+  }
+
+private:
+  typename string_set::value_type *pv;
+};
+
+} // namespace sona
+
+#endif // STRINGREF_HPP
---
+++ lib/sona/stringref.cc
@@ -0,0 +1,5 @@
+#include "sona/stringref.hpp"
+
+namespace impl_stc89c52 {
+thread_local std::unordered_map<std::string, int> glob_container;
+}
